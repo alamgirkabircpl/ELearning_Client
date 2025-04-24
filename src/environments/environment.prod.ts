14a82843a6f177d79b0348f092e5c202c5ca3b40
alamgircpl <alamgir.kabir@projukti-bd.com>
2025-04-24 18:27:36 +0600

    add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:44449', // Development API URL
+    apiUrl: 'https://elearning-fka2dpedhgbxh5hd.eastasia-01.azurewebsites.net', // Development API URL
+    // apiUrl: 'https://localhost:44449', // Development API URL
     googleMapsKey: 'prod-key-456',
     enableDebug: false,
 };
