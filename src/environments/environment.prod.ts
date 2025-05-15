fb9421cf754ad561676e43cee32787266575eba4
alamgircpl <alamgir.kabir@projukti-bd.com>
2025-05-15 11:24:44 +0600

    publish to azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://elearning-fka2dpedhgbxh5hd.eastasia-01.azurewebsites.net', // Development API URL
-    apiUrl: 'https://localhost:44449', // Development API URL
+    apiUrl: 'https://elearnapi.azurewebsites.net', // Production API URL
+
+    // apiUrl: 'https://localhost:44449', // Development API URL
     googleMapsKey: 'prod-key-456',
     enableDebug: false,
 };
